05be2a5e55a75546dc350a91829a3f655a13d779
Derick Cardin <derickc50@gmail.com>
2018-10-31 00:10:07 +0000

    Updated add user route and began working on update user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,9 +33,38 @@ router.post('/', (req, res) => {
     name,
     password,
     avatar
-  })
+  });
   
   newUser.save()
-})
+  .then(user => res.status(201).json(user))
+  .catch(err => {
+    res.status(500).json({message: err});
+  });
+});
+
+//DELETE USERS
+
+router.delete('/:name', (req, res) => {
+  // TODO: protected route ensure the user is the one deleting
+const { name } = req.params;
+  User.findOne({ name })
+  .then(user => {
+    console.log(user);
+    if(!user) {
+      return res.status(404).json({message: `User: ${name} not found`});
+    }
+    user.remove()
+    .then(() => res.status(204).json({message: "User successfully deleted"}))
+    .catch(err => res.status(500).json(err));
+    })
+  .catch(err => res.status(500).json({message: err}));
+});
+
+router.put('/:name', (req, res) => {
+  // findOne()
+  // .then(user) => {
+    // findOneAndUpdate()
+  
+  });
   
 module.exports = router;
